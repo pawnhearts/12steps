568bfbc6ff5e577619a08795940570f78b403f70
robotnaoborot <robotnaoborot@gmail.com>
2022-11-05 13:20:26 +0300

    init

--- guide/urls.py
+++ guide/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from guide import views
 
 urlpatterns = [
-    path('/steps/', login_required(views.StepListView.as_view()), name='step-list'),
-    path('/steps/<int:pk>/', login_required(views.StepDetailView.as_view()), name='step-detail'),
+    path('steps/', login_required(views.StepListView.as_view()), name='step-list'),
+    path('steps/<int:pk>/', login_required(views.StepDetailView.as_view()), name='step-detail'),
 ]
