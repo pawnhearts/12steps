e2cb36d485569e247c8d667bbce3cae6b1163ef7
robotnaoborot <robotnaoborot@gmail.com>
2022-11-05 13:31:45 +0300

    init

--- guide/urls.py
+++ guide/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
+from django.views.generic import TemplateView
+
 from guide import views
 
 urlpatterns = [
+    path('$', TemplateView.as_view(template_name='guide/index.html')),
     path('steps/', login_required(views.StepListView.as_view()), name='step-list'),
     path('steps/<int:pk>/', login_required(views.StepDetailView.as_view()), name='step-detail'),
 ]
