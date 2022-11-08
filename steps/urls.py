8f0a2a672c83978c8adfd9fe2f80ace3423615cc
robotnaoborot <robotnaoborot@gmail.com>
2022-11-08 11:35:46 +0300

    init

--- steps/urls.py
+++ steps/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.flatpages.views import flatpage
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('', include('guide.urls')),
-    path('', flatpage, {'url': '/'}, name='index'),
+    # path('', flatpage, {'url': '/'}, name='index'),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path("select2/", include("django_select2.urls")),
     path("admin/", admin.site.urls),
+    re_path(r'^(?P<url>.*/)$', flatpage),
 ]
