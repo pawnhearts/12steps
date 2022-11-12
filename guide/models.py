0b32e73206fcc263d25bfbc08c3d8e4621fca8d9
robotnaoborot <robotnaoborot@gmail.com>
2022-11-12 16:13:34 +0300

    init

--- guide/models.py
+++ guide/models.py
@@ -41,7 +41,6 @@ class Step(models.Model):
 class Section(models.Model):
     step = models.ForeignKey(Step, verbose_name='Шаг', on_delete=models.CASCADE)
     number = models.PositiveSmallIntegerField('Номер', blank=True, db_index=True)
-    show_header = models.BooleanField('Показывать заголовок', default=True)
     title = models.CharField('Название', max_length=256)
     text = HTMLField('Текст', blank=True, null=True)
 
