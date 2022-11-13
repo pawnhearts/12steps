911893bfe512814eb31aec043fa2474d6ef4cf90
robotnaoborot <robotnaoborot@gmail.com>
2022-11-13 09:46:03 +0300

    init

--- guide/urls.py
+++ guide/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('answer/<int:pk>/', views.AnswerUpdateView.as_view(), name='answer-update'),
     path('answer/<int:pk>/delete/', views.AnswerDeleteView.as_view(), name='answer-delete'),
     path('question/<int:pk>/close/', views.AnswerCloseView.as_view(), name='question-close'),
-    re_path(r'^vote/(?P<pk>\d+)/(?P<vote>up|down)/$', views.AnswerVoteView.as_view(), 'answer-vote'),
+    re_path(r'^vote/(?P<pk>\d+)/(?P<vote>up|down)/$', views.AnswerVoteView.as_view(), name='answer-vote'),
 ]
